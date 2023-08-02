7fa6b4a41f315269076e1c9a973b58df8e68daa5
Hobbes_ <ktudiver@googlemail.com>
2023-08-01 20:05:19 -0400

    added say function

--- objects/speech_bubble/speech_bubble.gd
+++ objects/speech_bubble/speech_bubble.gd
@@ -4,10 +4,25 @@ extends NinePatchRect
 @onready var timer = $TalkTimer
 
 
+func say(content: String, speaker: String) -> void:
+	label.hide()
+	format_with_text(content)
+	await get_tree().process_frame
+	await get_tree().process_frame
+	await get_tree().process_frame
+	label.visible_characters = 0
+	label.show()
+	for letter in content:
+		label.visible_characters += 1
+		timer.start()
+		await timer.timeout
+	label.visible_characters = -1
+
+
 func format_with_text(text: String) -> void:
 	label.text = text
 	
 	await get_tree().process_frame
 	
-	size.x = label.size.x
+	size.x = label.size.x + 1
 	size.y = label.size.y + 4
