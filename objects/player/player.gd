c57628d02f112312d47a2fb50b2c163d5260431c
4321booN <jackdgrabina@gmail.com>
2023-08-27 14:54:16 -0400

    fixed bug where player glitched through nodes above them while uncrouching, and added full collision for the fully dirt tile

--- objects/player/player.gd
+++ objects/player/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+const JUMP_VELOCITY: float = -250.0
+@export var speed: float = 150.0
+var bodies_in_headspace: int = 0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var sprint_speed: float = 175.0
 
 func _ready() -> void:
 	$AnimationPlayer.play("RESET")
@@ -17,13 +18,28 @@ func _physics_process(delta):
 
 	if Input.is_action_pressed("crouch"):
 		$AnimationPlayer.play("crouch")
+		$HeadspaceDetector.monitoring = true
 	else:
-		$AnimationPlayer.play_backwards("crouch")
+		if bodies_in_headspace > 0:
+			$AnimationPlayer.play("crouch")
+		else:
+			$AnimationPlayer.play_backwards("crouch")
+			$HeadspaceDetector.monitoring = false
 
 	var direction = Input.get_axis("walk_l", "walk_r")
 	if direction:
-		velocity.x = direction * SPEED
+		if Input.is_action_pressed("sprint"):
+			velocity.x = direction * sprint_speed
+		else:
+			velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
+
+
+func _on_headspace_detector_body_entered(body: Node2D) -> void:
+	bodies_in_headspace += 1
+
+func _on_headspace_detector_body_exited(body: Node2D) -> void:
+	bodies_in_headspace -= 1
