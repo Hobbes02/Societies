be832411d75968a65e09df01316d3dfc79f38024
4321booN <jackdgrabina@gmail.com>
2023-08-28 15:24:11 -0400

    added player walk, sprint and jump particles. Also made player slower while crouching

--- objects/player/player.gd
+++ objects/player/player.gd
@@ -2,31 +2,62 @@ extends CharacterBody2D
 
 signal get_tile_data
 enum TileTypes{
- EMPTY,
- GRAVEL,
- BLUE_STONE,
- CONCRETE,
- DAMAGED_CONCRETE,
- GRASS,
- DIRT
+	EMPTY,
+	GRAVEL,
+	BLUE_STONE,
+	CONCRETE,
+	DAMAGED_CONCRETE,
+	GRASS,
+	DIRT
 }
 const JUMP_VELOCITY: float = -225.0
 @export var speed: float = 95.0
 @export var at_feet_tile: int
-var tile_pos: Vector2
-var bodies_in_headspace: int = 0
-var walk_particle_color: Color = Color(0, 0, 0, 0)
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var sprint_speed: float = 140.0
+var crouch_speed: float = 50.0
+
+var walk_emit: int = 10
+var walk_emit_gravity: Vector2 = Vector2(0,400)
+var sprint_emit: int = 14
+var sprint_emit_gravity: Vector2 = Vector2(0,800)
+var particle_color: Color = Color(0,0,0,0)
+
+var tile_pos: Vector2
+
+var bodies_in_headspace: int = 0
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready() -> void:
 	$AnimationPlayer.play("RESET")
 
 func _physics_process(delta):
+	tile_pos = Vector2(position.x, position.y + 12)
+	emit_signal("get_tile_data")
+	print(at_feet_tile)
+	match at_feet_tile:
+		TileTypes.EMPTY:
+			particle_color = Color(0,0,0,0)
+		TileTypes.GRAVEL:
+			particle_color = Color("727784")
+		TileTypes.BLUE_STONE:
+			particle_color = Color("838997")
+		TileTypes.CONCRETE:
+			particle_color = Color("929292")
+		TileTypes.DAMAGED_CONCRETE:
+			particle_color = Color("8b8b8b")
+		TileTypes.GRASS:
+			particle_color = Color("458149")
+		TileTypes.DIRT:
+			particle_color = Color("8d6b52")
+	$WalkParticles.color = particle_color
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
+		$JumpParticles.emitting = true
+		$JumpParticles.emitting = false
+		$JumpParticles.restart()
 		velocity.y = JUMP_VELOCITY
 
 	if Input.is_action_pressed("crouch"):
@@ -39,21 +70,31 @@ func _physics_process(delta):
 			$AnimationPlayer.play_backwards("crouch")
 			$HeadspaceDetector.monitoring = false
 
-	tile_pos = Vector2(position.x, position.y + 12)
-	emit_signal("get_tile_data")
-	print(at_feet_tile)
-	
+	if (Input.is_action_pressed("walk_l") or Input.is_action_pressed("walk_r")) and is_on_floor():
+		$WalkParticles.restart()
+		$WalkParticles.emitting = true
+		if Input.is_action_pressed("sprint"):
+			$WalkParticles.amount = sprint_emit
+			$WalkParticles.gravity = sprint_emit_gravity
+		elif Input.is_action_pressed("crouch"):
+			$WalkParticles.emitting = false
+		else:
+			$WalkParticles.amount = walk_emit
+			$WalkParticles.gravity = walk_emit_gravity
+	elif Input.is_action_just_released("walk_l") or Input.is_action_just_released("walk_r") or not is_on_floor():
+		$WalkParticles.emitting = false
 	var direction = Input.get_axis("walk_l", "walk_r")
 	if direction:
 		if Input.is_action_pressed("sprint"):
 			velocity.x = direction * sprint_speed
+		elif Input.is_action_pressed("crouch"):
+			velocity.x = direction * crouch_speed
 		else:
 			velocity.x = direction * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
-
 
 func _on_headspace_detector_body_entered(body: Node2D) -> void:
 	bodies_in_headspace += 1
