14b83f2b1da28b0fa54b652f94db2e1de0f4e376
4321booN <jackdgrabina@gmail.com>
2023-08-29 18:20:51 -0400

    made most lines 'Safe' and commented code

--- objects/player/player.gd
+++ objects/player/player.gd
@@ -15,6 +15,7 @@ const JUMP_VELOCITY: float = -225.0
 @export var at_feet_tile: int
 var sprint_speed: float = 140.0
 var crouch_speed: float = 50.0
+var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var walk_emit: int = 10
 var walk_emit_gravity: Vector2 = Vector2(0,400)
@@ -30,54 +31,63 @@ var particle_color: Array = [
 	Color("8d6b52")
 ]
 
-
 var tile_pos: Vector2
 
 var bodies_in_headspace: int = 0
 
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
+@onready var walk_particles: CPUParticles2D = $WalkParticles
+@onready var jump_particles: CPUParticles2D = $JumpParticles
+@onready var headspace_detector: Area2D = $HeadspaceDetector
 
 func _ready() -> void:
-	$AnimationPlayer.play("RESET")
+	anim_player.play("RESET")
 
-func _physics_process(delta):
+func _physics_process(delta: float) -> void:
+#	Gets tile data from the tile at 'tile_pos'
 	tile_pos = Vector2(position.x, position.y + 12)
 	emit_signal("get_tile_data")
 	print(at_feet_tile)
-	$WalkParticles.color = particle_color[at_feet_tile]
+#	Sets the $WalkParticles color
+	walk_particles.color = particle_color[at_feet_tile]
+#	Falling
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
+#	Jumping and Jump particle emission
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		$JumpParticles.emitting = true
-		$JumpParticles.emitting = false
-		$JumpParticles.restart()
+		jump_particles.emitting = true
+		jump_particles.emitting = false
+		jump_particles.restart()
 		velocity.y = JUMP_VELOCITY
 
+#	Crouching
 	if Input.is_action_pressed("crouch"):
-		$AnimationPlayer.play("crouch")
-		$HeadspaceDetector.monitoring = true
+		anim_player.play("crouch")
+		headspace_detector.monitoring = true
 	else:
+#		Headspace Clearance
 		if bodies_in_headspace > 0:
-			$AnimationPlayer.play("crouch")
+			anim_player.play("crouch")
 		else:
-			$AnimationPlayer.play_backwards("crouch")
-			$HeadspaceDetector.monitoring = false
+			anim_player.play_backwards("crouch")
+			headspace_detector.monitoring = false
 
+#	Walking and Walk particle emission
 	if (Input.is_action_pressed("walk_l") or Input.is_action_pressed("walk_r")) and is_on_floor():
-		$WalkParticles.restart()
-		$WalkParticles.emitting = true
+		walk_particles.restart()
+		walk_particles.emitting = true
 		if Input.is_action_pressed("sprint"):
-			$WalkParticles.amount = sprint_emit
-			$WalkParticles.gravity = sprint_emit_gravity
+			walk_particles.amount = sprint_emit
+			walk_particles.gravity = sprint_emit_gravity
 		elif Input.is_action_pressed("crouch"):
-			$WalkParticles.emitting = false
+			walk_particles.emitting = false
 		else:
-			$WalkParticles.amount = walk_emit
-			$WalkParticles.gravity = walk_emit_gravity
+			walk_particles.amount = walk_emit
+			walk_particles.gravity = walk_emit_gravity
 	elif Input.is_action_just_released("walk_l") or Input.is_action_just_released("walk_r") or not is_on_floor():
-		$WalkParticles.emitting = false
-	var direction = Input.get_axis("walk_l", "walk_r")
+		walk_particles.emitting = false
+	var direction: int = Input.get_axis("walk_l", "walk_r")
 	if direction:
 		if Input.is_action_pressed("sprint"):
 			velocity.x = direction * sprint_speed
@@ -87,9 +97,9 @@ func _physics_process(delta):
 			velocity.x = direction * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
-
 	move_and_slide()
 
+#Headspace Clearance Detection
 func _on_headspace_detector_body_entered(body: Node2D) -> void:
 	bodies_in_headspace += 1
 
