e19b37dbf3ea6cc031488098e3c2d0364807d3b3
4321booN <jackdgrabina@gmail.com>
2023-10-07 16:27:29 -0400

    added NOT WORKING popup for changing keybinds and the 'medium_buttons.tres' and 'small_buttons.tres' styles

--- ui/menus/settings.gd
+++ ui/menus/settings.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var tabs: TabContainer = $TabContainer
+@onready var keybind_popup: Window = $TabContainer/Keybinds/ChangeKeybind
 
 
 func _ready() -> void:
@@ -9,3 +10,16 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_walk_left_change_primary_pressed() -> void:
+	keybind_popup.show()
+
+
+
+func _on_confirm_button_pressed() -> void:
+	keybind_popup.hide()
+
+
+func _on_cancel_button_pressed() -> void:
+	keybind_popup.hide()
