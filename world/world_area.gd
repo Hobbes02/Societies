42fbb6c1bebee2615b8c9288716f5fdfa5020eef
Hobbes_ <ktudiver@googlemail.com>
2023-10-13 17:17:07 -0400

    reduced world pixel scale

--- world/world_area.gd
+++ world/world_area.gd
@@ -32,4 +32,4 @@ func _process(delta: float) -> void:
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("debug"):
-		$NPCs/NPC.pathfind($Destinations/PathfindDestination)
+		pass
