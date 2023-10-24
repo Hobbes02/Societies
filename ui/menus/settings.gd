cb7b3102f8312073ef2e21d50444d26fa2c65378
Hobbes_ <ktudiver@googlemail.com>
2023-10-24 16:27:40 -0400

    removed redundant files

--- ui/menus/settings.gd
+++ ui/menus/settings.gd
@@ -5,8 +5,3 @@ extends Control
 
 func _ready() -> void:
 	tabs.current_tab = Settings.tab
-
-
-func _process(delta: float) -> void:
-	if tabs.current_tab == 3:
-		get_tree().change_scene_to_file("res://ui/menus/main_menu.tscn")
