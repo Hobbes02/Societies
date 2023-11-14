ba7ae5fe32210fbc5d097d5bed3463e1046df216
Hobbes_ <ktudiver@googlemail.com>
2023-11-13 21:39:28 -0500

    snapped door open/close animations to the pixel

--- objects/buildings/building.gd
+++ objects/buildings/building.gd
@@ -1,23 +1,52 @@
 extends Node2D
 
-@export var interior: PackedScene
+@export_file("*.tscn") var interior: String = ""
+
+@export_group("New Building Settings")
+@export var door_width: int = 8
+@export var door_open_frame_length: float = 0.1
 
 var can_enter: bool = false
+var door_opening: bool = false
+var door_closing: bool = false
 
-@export var animation_player: AnimationPlayer
-
+@onready var door: Sprite2D = $CanvasGroup/Door
+@onready var default_door_position: Vector2 = door.global_position
 
 func _input(event: InputEvent) -> void:
 	if can_enter and event.is_action_pressed("interact"):
-		# TODO: Save game
-		get_tree().change_scene_to_packed(interior)
+		SaveManager.about_to_save.emit()
+		SceneManager.change_scene(interior)
 
 
 func _on_doorway_body_entered(body: Node2D) -> void:
-	animation_player.play("open_door")
+	can_enter = true
+	await open_door()
 	can_enter = true
 
 
 func _on_doorway_body_exited(body: Node2D) -> void:
-	animation_player.play_backwards("open_door")
+	await close_door()
 	can_enter = false
+
+
+func open_door() -> void:
+	door_opening = true
+	door_closing = false
+	for frame in abs((default_door_position.x - door_width) - door.global_position.x):
+		if door_closing or not door_opening:
+			break
+		door.global_position.x -= 1
+		await get_tree().create_timer(door_open_frame_length).timeout
+	door_opening = false
+
+
+func close_door() -> void:
+	door_closing = true
+	door_opening = false
+	for frame in abs(default_door_position.x - door.global_position.x):
+		if door_opening or not door_closing:
+			break
+		door.global_position.x += 1
+		await get_tree().create_timer(door_open_frame_length).timeout
+	door_closing = false
